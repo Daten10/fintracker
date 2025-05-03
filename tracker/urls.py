d365d060fcd927f4796049acf99a3f1e28393236
Daten10 <tendanil563@gmail.com>
2025-05-03 19:51:25 +0600

    v5

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,5 +4,5 @@ from .views import add_transaction, get_stats,get_transactions
 urlpatterns = [
     path('add_transaction/', add_transaction),
     path('get_stats/', get_stats),
-    path('api/get_transactions/', get_transactions),
+    path('get_transactions/', get_transactions),
 ]
